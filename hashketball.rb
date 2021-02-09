f761a2adca2fa6b3b0f3da4e2f14ff923423d702
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 23:05:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash 
   game_hash = {
-    :home => {
-      :team_name => "Brooklyn Nets",
+    :home=> {
+      :team_name=> "Brooklyn Nets",
       :colors ["Black", "White"],
       :players [
         {
@@ -63,7 +63,7 @@ def game_hash
     },
     
     :away => {
-      :team_name => "Charlotte Hornets",
+      :team_name=> "Charlotte Hornets",
       :colors ["Turquoise","Purple"],
       :players [
         {
